5004de1ec72f4100d8e84041037d837a41b638d3
Thom Krynen <thom@land.tech>
2022-05-18 15:21:07 +0900

    Add extract function

---
+++ extract.py
@@ -0,0 +1,35 @@
+import requests
+import config as cfg
+
+
+def get_latest_data(state):
+    url = cfg.FEMA_REQUEST_URL
+    state_code = cfg.STATE_FIPS_CODES[state]
+    data = cfg.FEMA_REQUEST_DATA
+    data["selstate"] = state_code
+
+    response = requests.post(
+                url=url,
+                data=data
+                )
+
+    data = response.json()
+    latest_data = data["EFFECTIVE"]["NFHL_STATE_DATA"][0]["product_FILE_PATH"]
+    
+    return latest_data
+
+
+def download_latest_data(filename):
+    url = f"https://hazards.fema.gov/nfhlv2/output/State/{filename}"
+    print(f"Downloading filename from {url}...")
+    requests.get(url)
+
+
+def main():
+    state = "FL"
+    latest_data = get_latest_data(state)
+    download_latest_data(latest_data)
+
+
+main()
+
